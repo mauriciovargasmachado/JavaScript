c744fe6dcf960d45b2746656107a9ea8cf32a1c6
Mauricio Vargas <mauriciovargasmachado@gmail.com>
2023-10-02 18:33:01 -0400

    Changes in HTML Octuber 2023

---
+++ October/Web_Ejemplo/script.js
@@ -0,0 +1,55 @@
+//console.log('This is a test!')
+
+//let name = 'Miguel'
+//const lastName = 4
+// var isDeveloper = true
+
+//let data = (name+lastName)
+
+//let dataUpper = data.toUpperCase()
+
+//console.log (dataUpper)
+
+//let list = [1,2,3,4]
+//list.push(5)
+//console.log(list)
+
+//console.log(list[2])
+
+//let list_2 = list.concat(6)
+
+//console.log(list_2)
+
+// const persona_1 = {
+//     name: 'David',
+//     lastName: 'Olivares',
+//     PhoneNumber:357886557,
+//     links:['www.youtube.com','www.google.com']
+// }
+
+// console.log(persona_1)
+// console.log(persona_1.name)
+// console.log(persona_1.lastName)
+// console.log(persona_1.PhoneNumber)
+// console.log(persona_1.links[0])
+// console.log(persona_1.links[1])
+
+const sumar = function(valor_1,valor_2){
+    console.log(valor_1)
+    console.log(valor_2)
+    resultado = valor_1+valor_2
+    console.log(resultado) 
+}
+
+function restar(valor_1,valor_2){
+    console.log(valor_1)
+    console.log(valor_2)
+    resultado = valor_1-valor_2
+    console.log(resultado) 
+}
+
+valor1 = 56
+valor2 =852
+
+sumar(valor1,valor2)
+restar(valor1,valor2)
