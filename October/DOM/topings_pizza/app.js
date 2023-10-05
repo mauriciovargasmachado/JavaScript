048182dd072ff5634abb5e718f62126b8ff75d4f
Mauricio Vargas <mauriciovargasmachado@gmail.com>
2023-10-05 16:39:06 -0400

    Cambios en DOM

---
+++ October/DOM/topings_pizza/app.js
@@ -0,0 +1,8 @@
+
+const contenedor = document.getElementById('contenedor');
+console.log(contenedor);
+const titulo = document.getElementById('titulo');
+console.log(titulo);
+console.log(titulo.innerText);
+console.log(titulo.tagName);
+
