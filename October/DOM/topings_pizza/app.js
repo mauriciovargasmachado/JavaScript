068d2c051c7cfad2a8f22cffb7d4ed4e53658cb6
Mauricio Vargas <mauriciovargasmachado@gmail.com>
2023-10-06 12:12:31 -0400

    Changes in toppings_pizza

--- October/DOM/topings_pizza/app.js
+++ October/DOM/topings_pizza/app.js
@@ -6,3 +6,31 @@ console.log(titulo);
 console.log(titulo.innerText);
 console.log(titulo.tagName);
 
+const topping = document.getElementsByClassName('topping');
+console.log(topping[0],topping[4]);
+
+
+const mistoppings = document.getElementsByTagName('li');
+console.log(mistoppings);
+
+const fondoMarron = document.getElementsByClassName('fondo-marron');
+console.log(fondoMarron);
+
+const aceituna = document.querySelector('#aceitunas');
+console.log(typeof aceituna);
+console.log(aceituna);
+
+const elements_1 = document.querySelector('ul li:not(.fondo-marron)');
+console.log(typeof elements_1);
+console.log(elements_1);
+
+const elements_2 = document.querySelectorAll('.fondo-naranja');
+console.log(typeof elements_2);
+console.log(elements_2);
+console.log(elements_2.length);
+
+const cambiarFondoAceitunas = document.querySelector('.topping');
+cambiarFondoAceitunas.style.backgroundColor = 'blue';
+cambiarFondoAceitunas.style.textTransform = 'upperCase';
+cambiarFondoAceitunas.style.color = '#6dff00';
+console.log(cambiarFondoAceitunas.style);
